0b722724ee31018e5d6dd7e146b539c935596105
Stéphane Dryefus <floatingrock@gmail.com>
2019-01-23 14:13:41 -0800

    Basic program finished. Need counter

--- memory.js
+++ memory.js
@@ -1,28 +1,68 @@
-// console.log("What is document?", document)
 window.onload = function(){
 const cards = document.querySelectorAll('.memory-card');
-// console.log("JS started");
-
-// console.log("What is cards", cards);
-// console.log("Trying to pull thing.", document.querySelectorAll('section'));
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
+// let moveCounter = 0;
+
+(function shuffle(){
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})()
 
 function flipCard(){
-    console.log("flipcard activated")
+    if (lockBoard) return;
+    if (this === firstCard) return;
+    // counterUpdate(++moveCounter);
+    // Do not yet know how to get this back to HTML
     this.classList.add('flip');
+    if (this === firstCard) return;
     if (!hasFlippedCard){
         hasFlippedCard = true;
         firstCard = this;
+        return;
     }
+    secondCard = this;
+
+    let isMatch = (firstCard.dataset.framework === secondCard.dataset.framework);
+    isMatch ? disableCards() : unflipCards();
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
 
-// cards.forEach(function(card){
-//     card.addEventListener('click', flipCard);
-// })
-// Can't seem to get either of these to work.
+function unflipCards(){
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1500);
 }
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+// function counterUpdate(num){
+//     // Sends counter to display.
+//     return moveCounter;
+// }
+
+// cards.forEach(card => card.addEventListener('click', flipCard));
+
+cards.forEach(function(card){
+    card.addEventListener('click', flipCard);
+})
+
+}
+// Last bracket end of wrapping onload function.
 
