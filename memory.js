2dad969fb1fa47bb4109aba61aa56f6aada0b3ff
Stéphane Dreyfus <floatingrock@gmail.com>
2023-05-19 11:11:54 -0700

    Change to import card faces.

--- memory.js
+++ memory.js
@@ -1,27 +1,16 @@
 // Small function sent to testing to ensure page load.
 const add = (a, b) => a + b;
 
+const faceArrays = require('./memory');
+
 window.onload = function memoryGame() {
     
 // Array of card face images
-const cardFaces = [
-    {title: "Adulthood", file: "Adulthood.png"},
-    {title: "Hak061", file: "Hak061.png"},
-    {title: "Hak279", file: "Hak279.png"},
-    {title: "Kandinsky", file: "kandinsky.png"},
-    {title: "Mondrian2", file: "mondrian2.png"},
-    {title: "Originedumonde", file: "originedumonde.png"},
-    {title: "Picasso2", file: "Picasso2.png"},
-    {title: "Delauney", file: "delauney.png"},
-    {title: "Flower4", file: "Flower4.svg"},
-    {title: "Rhombille", file: "Rhombille.svg"},
-    {title: "Star_ball_5", file: "Star_ball_5.svg"},
-    {title: "Triring41", file: "Triring41.svg"}
-];
+const faces = faceArrays.hilma;
 
 function createCards() {
     const board = document.getElementById("board");
-    for (face of cardFaces) {
+    for (face of faces) {
         // Create parts of card
         const c = document.createElement("div");
         const f = document.createElement("img");
